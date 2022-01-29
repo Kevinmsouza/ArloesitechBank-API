2992d48e92d7da97746f99a6477e67521cf7c427
Kevinmsouza <kevinm.souza@hotmail.com>
2022-01-29 12:59:28 -0400

    fix: accountSchema regex

--- src/schemas/accountSchema.ts
+++ src/schemas/accountSchema.ts
@@ -1,6 +1,6 @@
 import joi from "joi";
 
 export default joi.object({
-  number: joi.string().min(5).required(),
-  agency: joi.string().min(3).required()
+  number: joi.string().pattern(/^([0-9]+)-([0-9]|X)$/).length(7).required(),
+  agency: joi.string().pattern(/^[0-9]+$/).length(4).required()
 });
